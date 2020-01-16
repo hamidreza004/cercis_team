f603d28eec0a75555f3000552567da135dbc6f95
amiraz <amiraazarmehr@gmail.com>
2020-01-16 22:11:24 +0330

    - Created Cell and cells array - Initializing neighbours - Created the SenseNeighbour function

--- src/ufo/RobotPlayer.java
+++ src/ufo/RobotPlayer.java
@@ -1,6 +1,10 @@
 package ufo;
 
 import battlecode.common.*;
+import com.sun.tools.javac.util.Pair;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public strictfp class RobotPlayer {
 
@@ -13,16 +17,10 @@ public strictfp class RobotPlayer {
         MINE_SOUP,
     }
 
-    enum Cell {
-        NOT_DISCOVERED,
-        NOTHING,
-        SOUP,
-        HQ,
-        REFINERY,
-        DESIGN_SCHOOL,
-        FULFILLMENT_CENTER,
-        NET_GUN;
-        boolean team;
+    static class Cell{
+        boolean flooded, visited;
+        RobotType building;
+        int elevation, soup;
     }
 
     static Direction[] directions = {
@@ -30,38 +28,74 @@ public strictfp class RobotPlayer {
             Direction.SOUTH,
             Direction.SOUTHEAST,
             Direction.WEST,
-            null,
+            Direction.CENTER,
             Direction.EAST,
             Direction.NORTHWEST,
             Direction.NORTH,
             Direction.NORTHEAST,
     };
 
+    static RobotType[] spawnedByMiner = {RobotType.REFINERY, RobotType.VAPORATOR, RobotType.DESIGN_SCHOOL,
+            RobotType.FULFILLMENT_CENTER, RobotType.NET_GUN};
+
+
     static RobotController rc;
 
-    State state = State.NO_STATE;
+    static State state = State.NO_STATE;
 
     static int turnCount;
 
-    Cell[][] saveMap = new Cell[64][64];
+    static Cell[][] cells;
+
+    static ArrayList< Pair<Integer, Integer> > neighbours = new ArrayList<>();
 
     //Helpers:
 
-    void setInSaveMap(MapLocation mapLocation, Cell cell) {
-        saveMap[mapLocation.x][mapLocation.y] = cell;
+    static boolean isBuilding(RobotType robotType){
+        return (robotType == RobotType.DESIGN_SCHOOL ||
+            robotType == RobotType.FULFILLMENT_CENTER ||
+            robotType == RobotType.VAPORATOR ||
+            robotType == RobotType.REFINERY ||
+            robotType == RobotType.HQ);
     }
 
-    Cell getFromSaveMap(MapLocation mapLocation) {
-        return saveMap[mapLocation.x][mapLocation.y];
+    static void setCell(MapLocation mapLocation, Cell cell) {
+        cells[mapLocation.x][mapLocation.y] = cell;
+    }
+
+    static Cell getCell(MapLocation mapLocation) {
+        return cells[mapLocation.x][mapLocation.y];
     }
 
     static int distance(MapLocation a, MapLocation b) {
         return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
     }
 
+    private static void getNeighbours() {
+        int r = rc.getType().sensorRadiusSquared;
+        for (int dx = -r; dx <= r; dx++)
+            for (int dy = -r; dy <= r; dy++)
+                if (dx * dx + dy * dy <= r)
+                    neighbours.add(new Pair<>(dx, dy));
+    }
+
     public static void run(RobotController rc) throws GameActionException {
         RobotPlayer.rc = rc;
         turnCount = 0;
+
+        cells = new Cell[rc.getMapWidth()][rc.getMapHeight()];
+        for (int i = 0; i < rc.getMapWidth(); i++)
+            for (int j = 0; j < rc.getMapHeight(); j++) {
+                cells[i][j] = new Cell();
+            }
+
+        try {
+            getNeighbours();
+        }catch(Exception e){
+            System.out.println(rc.getType() + " Exception");
+            e.printStackTrace();
+        }
+
 
         while (true) {
             turnCount += 1;
@@ -104,7 +138,30 @@ public strictfp class RobotPlayer {
         }
     }
 
+    static void senseNeighbours() throws GameActionException {
+        for (Pair<Integer, Integer> d : neighbours) {
+            MapLocation loc = rc.getLocation().translate(d.fst, d.snd);
+            if (rc.canSenseLocation(loc)){
+                Cell cell = getCell(loc);
+                cell.visited = true;
+                cell.elevation = rc.senseElevation(loc);
+                cell.flooded = rc.senseFlooding(loc);
+                cell.soup = rc.senseSoup(loc);
+            }
+        }
+
+        for (RobotInfo info : rc.senseNearbyRobots())
+            if (info.getTeam() != rc.getTeam() && info.getTeam() != Team.NEUTRAL && isBuilding(info.getType())){
+                getCell(info.getLocation()).building = info.getType();
+                /*if (info.getType() == RobotType.HQ) {
+                    found enemy HQ
+                }*/
+            }
+    }
+
     static void runHQ() throws GameActionException {
+        if (turnCount == 1)
+            senseNeighbours();
     }
 
     static void runMiner() throws GameActionException {
